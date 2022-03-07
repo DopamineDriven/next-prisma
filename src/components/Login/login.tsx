68543f81d08259f1b05f6d65b0dd5e6ab8dc400d
Andrew Ross <andrew.ross@cortinahealth.com>
2022-03-07 04:45:24 -0600

    stashing updates -- building out serverside context with authed user for rapid fire data population

--- src/components/Login/login.tsx
+++ src/components/Login/login.tsx
@@ -89,14 +89,16 @@ export default function Login() {
                 <Logo className={cn(css.logo)} />
               </a>
             </Link>
-            <h1 className='lg:text-[1.025rem] text-[0.8rem] absolute -top-[7.75rem] lg:-top-[5.75rem] tracking-[0.25rem] leading-[2.34rem] font-extralight font-interVar text-stone-200'>
-              Next. Prisma. Mongo. Nexus. Apollo. GraphQL. Tailwind. Init.
+            <h1 className='lg:text-[1.025rem] text-[0.45rem] 5xs:text-[0.50rem] 4xs:text-[0.625rem] 3xs:text-[0.70rem] 2xs:text-[0.8rem] absolute -top-[7.75rem] lg:-top-[5.75rem] tracking-[0.25rem] leading-[2.34rem] font-extralight font-interVar text-stone-200'>
+              {"Next. Prisma. Mongo. Nexus."}
+              <br />
+              {"Apollo. GraphQL. Tailwind. Init."}
             </h1>
-            <h2 className='lg:text-[1.025rem] text-[0.75rem] absolute -top-5 lg:-top-3 tracking-[0.25rem] leading-[2.54rem] font-light font-interVar text-stone-300'>
+            <h2 className='lg:text-[1.025rem] text-[0.40rem] 5xs:text-[0.48rem] 4xs:text-[0.55rem] 3xs:text-[0.65rem] 2xs:text-[0.75rem] absolute -top-5 lg:-top-[1.0125rem] tracking-[0.25rem] leading-[2.54rem] font-light font-interVar text-stone-300'>
               <span className=''>Select a provider to get started.</span>
             </h2>
           </div>
-          <div className='mt-16 relative'>
+          <div className='my-16 relative'>
             <div>
               <div>
                 <div className={css.mapped}>
@@ -138,6 +140,11 @@ export default function Login() {
                 </div>
               </div>
             </div>
+            {/* <div className='hover:tracking-[0.25rem] hover:leading-[2.34rem]  font-extralight hover:text-stone-200 hover:relative hover:origin-right hover:right-40 opacity-0 hover:opacity-100 hover:delay-500 hover:transition-all hover:transform-gpu hover:duration-[2000ms] font-interVar hover:ease-linear hover:font-light'>
+              <span className="absolute left-1/2">
+              {"The mechanic that would perfect his work must first sharpen his tools."} <br /> {"- Confucius"}
+              </span>
+            </div> */}
           </div>
         </div>
       </div>
@@ -151,7 +158,7 @@ export default function Login() {
           height={Bg.height}
           quality='100'
           priority={true}
-          className='absolute inset-0 top-50 w-full object-contain mix-blend-exclusion filter-grayscale'
+          className='absolute inset-0 top-50 w-full min-h-screen object-cover mix-blend-exclusion filter-grayscale '
           src={Bg.src}
           alt='Login'
         />
